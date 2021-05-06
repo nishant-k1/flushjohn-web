6d1c305cb85407c483a59669d32d53a530d73783
Nishant Kumar <nishant290195.nkr@gmail.com>
2021-05-06 06:26:26 -0400

    implement pwa

--- pages/_app.js
+++ pages/_app.js
@@ -14,14 +14,21 @@ import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
-    ReactGA.initialize('UA-181579330-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.initialize('UA-181579330-1')
+    ReactGA.pageview(window.location.pathname + window.location.search)
   });
 
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="manifest" href="manifest.json"></link>
+        <link rel="icon" href="logo-black.svg"></link>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+        <meta name="theme-color" content="#fff" />
+        <meta name = "apple-mobile-web-app-capable" content="yes" />
+        <meta name = "apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name = "apple-mobile-web-app-title" content="Next PWA demo" />
       </Head>
       <DefaultSeo {...SEO}/>
       <SidebarContextProvider>
