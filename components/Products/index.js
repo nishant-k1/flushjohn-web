2a59d386d69ec78ec7a56430e41c59b9cc6e4e79
Nishant Kumar <nishant.29k1@gmail.com>
2023-04-26 02:28:14 +0530

    fix products

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { products_data } from "../constants";
-import productStyles from "../styles/Products.module.css";
-import Slider from "../components/Slider";
+import { products_data } from "../../constants";
+import styles from "./styles.module.css";
+import Slider from "../Slider";
 
 const Products = () => (
   <React.Fragment>
-    <div
-      className={`${productStyles.section} ${productStyles.section4Section}`}
-    >
-      <div className={productStyles.container}>
-        <div className={productStyles.section4}>
-          {/* <div className={productStyles.wrapper}>
+    <div className={`${styles.section} ${styles.section4Section}`}>
+      <div className={styles.container}>
+        <div className={styles.section4}>
+          {/* <div className={styles.wrapper}>
             <Slider
               img={{
                 img1: "assets/sps_yellow.svg",
@@ -20,7 +18,7 @@ const Products = () => (
             <h5>{products_data.h1}</h5>
             <p>{products_data.p1}</p>
           </div> */}
-          <div className={productStyles.wrapper}>
+          <div className={styles.wrapper}>
             {/* <img src="/assets/sps_blue.svg" alt="product_pic" /> */}
             <Slider
               img={{
@@ -31,7 +29,7 @@ const Products = () => (
             <h5>{products_data.h2}</h5>
             <p>{products_data.p2}</p>
           </div>
-          <div className={productStyles.wrapper}>
+          <div className={styles.wrapper}>
             {/* <img src="/assets/ada_blue.svg" alt="product_pic" /> */}
             <Slider
               img={{
@@ -42,7 +40,7 @@ const Products = () => (
             <h5>{products_data.h3}</h5>
             <p>{products_data.p3}</p>
           </div>
-          <div className={productStyles.wrapper}>
+          <div className={styles.wrapper}>
             {/* <img src="/assets/flushable.svg" alt="product_pic" /> */}
             <Slider
               img={{
@@ -53,7 +51,7 @@ const Products = () => (
             <h5>{products_data.h4}</h5>
             <p>{products_data.p4}</p>
           </div>
-          <div className={productStyles.wrapper}>
+          <div className={styles.wrapper}>
             {/* <img src="/assets/sink.svg" alt="product_pic" /> */}
             <Slider
               img={{ img1: "assets/sinkFront.png", img2: "assets/sink.png" }}
@@ -61,7 +59,7 @@ const Products = () => (
             <h5>{products_data.h5}</h5>
             <p>{products_data.p5}</p>
           </div>
-          {/* <div className={productStyles.wrapper}>
+          {/* <div className={styles.wrapper}>
             <Slider
               img={{
                 img1: "assets/VIP-Flush-1.svg",
