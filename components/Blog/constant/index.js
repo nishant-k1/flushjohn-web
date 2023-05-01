d54eae689fd10370421f6c92d2a7da96aaf81c05
Nishant Kumar <nishant.29k1@gmail.com>
2023-05-02 05:23:19 +0530

    fix blog issue

---
+++ components/Blog/constant/index.js
@@ -0,0 +1,84 @@
+export const post_data = {
+  post_list: [
+    {
+      id: 1,
+      date: "05/02/2023",
+      image: {
+        src: "/assets/products/sps_blue.svg",
+        alt: "Standard Portable Restroom",
+      },
+      title: "Standard Portable Restroom",
+      desc: "A standard porta potty is a portable toilet that is perfect for outdoor events and construction sites, providing basic amenities and convenience for users.",
+    },
+    {
+      id: 2,
+      date: "05/02/2023",
+      image: {
+        src: "/assets/products/ada_blue.svg",
+        alt: "ADA-Compliant Portable Restroom",
+      },
+      title: "ADA-Compliant Portable Restroom",
+      desc: "The Americans with Disabilities Act (ADA) requires that all public events and construction sites provide accessible portable toilets, also known as ADA compliant porta potties, to ensure that people with disabilities have equal access to facilities.",
+    },
+    {
+      id: 3,
+      date: "05/02/2023",
+      image: {
+        src: "/assets/products/flushable.svg",
+        alt: "Flushable Restroom-Sink inside",
+      },
+      title: "Flushable Restroom-Sink inside",
+      desc: "A deluxe flushable porta potty is a premium portable toilet that features flushing capability, freshwater supply, and a built-in sink with running water, providing users with a comfortable and convenient restroom experience similar to that of a traditional indoor restroom.",
+    },
+    {
+      id: 4,
+      date: "05/02/2023",
+      image: {
+        src: "/assets/products/sinkFront.png",
+        alt: "Hand Wash Sink Station",
+      },
+      title: "Hand Wash Sink Station",
+      desc: "A standalone handwash sink is a portable sink unit that is often used in outdoor events and construction sites, providing a convenient and hygienic handwashing solution for users to maintain cleanliness and prevent the spread of germs",
+    },
+  ],
+  spr: {
+    id: 1,
+    date: "05/02/2023",
+    image: {
+      src: "/assets/products/sps_blue.svg",
+      alt: "Standard Portable Restroom",
+    },
+    title: "Standard Portable Restroom",
+    desc: "A standard porta potty is a portable toilet that is perfect for outdoor events and construction sites, providing basic amenities and convenience for users.",
+  },
+  acpr: {
+    id: 2,
+    date: "05/02/2023",
+    image: {
+      src: "/assets/products/ada_blue.svg",
+      alt: "ADA-Compliant Portable Restroom",
+    },
+    title: "ADA-Compliant Portable Restroom",
+    desc: "The Americans with Disabilities Act (ADA) requires that all public events and construction sites provide accessible portable toilets, also known as ADA compliant porta potties, to ensure that people with disabilities have equal access to facilities.",
+  },
+  dfr: {
+    id: 3,
+    date: "05/02/2023",
+    image: {
+      src: "/assets/products/flushable.svg",
+      alt: "Flushable Restroom-Sink inside",
+    },
+    title: "Flushable Restroom-Sink inside",
+    desc: "A deluxe flushable porta potty is a premium portable toilet that features flushing capability, freshwater supply, and a built-in sink with running water, providing users with a comfortable and convenient restroom experience similar to that of a traditional indoor restroom.",
+  },
+  hss: {
+    id: 4,
+    date: "05/02/2023",
+    image: {
+      src: "/assets/products/sinkFront.png",
+      alt: "Hand Wash Sink Station",
+    },
+    title: "Hand Wash Sink Station",
+    desc: "A standalone handwash sink is a portable sink unit that is often used in outdoor events and construction sites, providing a convenient and hygienic handwashing solution for users to maintain cleanliness and prevent the spread of germs",
+  },
+};
--- components/Blog/index.js
+++ components/Blog/index.js
@@ -2,14 +2,30 @@ import React from "react";
 import styles from "./styles.module.css";
 import Breadcrumbs from "../Breadcrumbs";
 import { useRouter } from "next/router";
+import { post_data } from "./constant";
+import Link from "next/link";
 
 const Blog = () => {
   const router = useRouter();
+  const { post_list } = post_data;
+
   return (
     <div className={styles.blog}>
       <div className={styles.container}>
         <Breadcrumbs path={router.pathname} />
-        <div className={styles.wrapper}>Blog Post List</div>
+        <div className={styles.wrapper}>
+          <h1>Blog</h1>
+          {post_list.map((item, index) => {
+            const { date, title, desc } = item;
+            return (
+              <Link href={`/blog/${title.toLowerCase().replaceAll(" ", "-")}`}>
+                <h2>{title}</h2>
+                <h3>{date}</h3>
+                <p>{desc}</p>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
