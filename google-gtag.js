829cc599f4014b37113da99a35adb8f88dc66f87
Nishant Kumar <nishant.kumar@dowjones.com>
2023-07-04 19:36:38 +0530

    google analytics

--- google-gtag.js
+++ google-gtag.js
@@ -6,17 +6,34 @@ export const loadGtag = () => {
     return; // Don't load the tag if not in production
   }
 
-  // Load the gtag.js library asynchronously
-  const script = document.createElement("script");
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
-  script.async = true;
-  document.head.appendChild(script);
+  if (typeof window !== "undefined") {
+    // Load the gtag.js library asynchronously
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
 
-  // Initialize and configure the tag
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
+    // Initialize and configure the tag
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function gtag() {
+      window.dataLayer.push(arguments);
+    };
+    window.gtag("js", new Date());
+    window.gtag("config", GA_TRACKING_ID);
   }
-  gtag("js", new Date());
-  gtag("config", GA_TRACKING_ID);
 };
+
+// Function to send events to Google Analytics
+export const gtag = (...args) => {
+  if (window.gtagInitialized) {
+    window.gtag(...args);
+  }
+};
+
+// Set gtagInitialized flag when the script is loaded
+if (typeof window !== "undefined") {
+  window.gtagInitialized = false;
+  window.addEventListener("load", () => {
+    window.gtagInitialized = true;
+  });
+}
