ee73dabb406c10df541c0f3d2e5c59386d647c53
Nishant <nishant.29k1@gmail.com>
2024-09-26 03:36:47 +0530

    fix dynamic page issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import BlogPost from "@/components/Blog/Post";
 
 const BlogPostPage = ({ params }: { params: { slug: string } }) => {
-  console.log("params", params);
   const { slug } = params;
 
   return <BlogPost slug={slug} />;
