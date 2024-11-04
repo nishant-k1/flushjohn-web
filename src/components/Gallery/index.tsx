257f739eb8e0817786d7ff50d85226b51368f252
Nishant <nishant.29k1@gmail.com>
2024-11-04 08:23:08 +0530

    fsdfds

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Breadcrumbs from "../Breadcrumbs";
+import Image from "next/image";
+import { galleryImages } from "./galleryImages";
 
 const Gallery = () => {
   return (
@@ -8,7 +10,20 @@ const Gallery = () => {
       <div className={styles.gallery}>
         <div className={styles.container}>
           <Breadcrumbs path={""} />
-          <div className={styles.disclaimerWrapper}></div>
+          <div className={styles.imagesWrapper}>
+            {galleryImages.map(({ url, alt, id, height, width }) => {
+              return (
+                <div key={id}>
+                  <Image
+                    src={url}
+                    alt={alt}
+                    height={height}
+                    width={width}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </React.Fragment>
