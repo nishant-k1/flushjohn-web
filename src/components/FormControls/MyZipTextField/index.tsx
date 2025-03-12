0b32f005b29e734fa1b4e00425d7bd865bac9634
Nishant Kumar <nishant.29k1@gmail.com>
2025-03-13 03:48:21 +0530

    fix zip issue

--- src/components/FormControls/MyZipTextField/index.tsx
+++ src/components/FormControls/MyZipTextField/index.tsx
@@ -4,33 +4,11 @@ import React from "react";
 import { useField } from "formik";
 import { Tooltip } from "antd";
 import styles from "../../QuickQuote/styles.module.css";
-import "react-phone-number-input/style.css";
-import PhoneInput from "react-phone-number-input";
 
 const MyZipTextField = ({ label, ...props }: any) => {
   const [field, meta, helpers] = useField(props);
   const { touched, error } = meta;
   const { setValue } = helpers;
-
-  const USFlag = () => (
-    <div
-      role="img"
-      aria-label={"USA"}
-      style={{
-        fontSize: "20px",
-        marginRight: "8px",
-        display: "flex",
-        alignItems: "center",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <img
-        src="https://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg"
-        alt="us-flag"
-      />
-    </div>
-  );
 
   return (
     <div className={styles.inputWrapper}>
@@ -38,24 +16,18 @@ const MyZipTextField = ({ label, ...props }: any) => {
         placement="top"
         title="Enter 5-digit zip code"
       >
-        <PhoneInput
+        <input
           {...field}
           {...props}
+          type="text"
           className={styles.input}
-          autoComplete={"tel"}
-          onChange={(value) => setValue(value)}
-          limitMaxLength={true}
           maxLength={5}
           inputMode="numeric"
-          initialValueFormat={false}
-          addInternationalOption={false}
-          international={false}
-          flagComponent={USFlag}
-          // defaultCountry="US"
-          // countries={["US"]}
-          displayInitialValueAsLocalNumber={false}
-
-          // countryCallingCodeEditable={false}
+          pattern="[0-9]*"
+          onChange={(e) => {
+            const value = e.target.value.replace(/\D/g, "").slice(0, 5);
+            setValue(value);
+          }}
         />
       </Tooltip>
       {touched && error && <div className={styles.error}>{error}</div>}
